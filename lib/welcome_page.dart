52789692012aae210c8bd3a6c422ff0388f77025
Wricheek <wricheekadhikari1@gmail.com>
2021-08-11 18:41:12 +0530

    initial commit

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'utilities/Mediaquery.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomePage extends StatefulWidget {
-  WelcomePage({Key key, this.title}) : super(key: key);
-
+  WelcomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
@@ -11,6 +11,85 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  Widget _login() {
+    return InkWell(
+      onTap: () {
+        //  Navigator.push(
+        // context, MaterialPageRoute(builder: (context) => LoginPage()));
+      },
+      child: Container(
+        width: getDeviceWidth(context),
+        padding: EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Color(0xffdf8e33).withAlpha(100),
+                  offset: Offset(2, 4),
+                  blurRadius: 8,
+                  spreadRadius: 2)
+            ],
+            color: Colors.white),
+        child: Text(
+          'Login',
+          style: TextStyle(fontSize: 20, color: Color(0xfff7892b)),
+        ),
+      ),
+    );
+  }
+
+  Widget _signUpButton() {
+    return InkWell(
+      onTap: () {
+        //  Navigator.push(
+        // context, MaterialPageRoute(builder: (context) => SignUpPage()));
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          border: Border.all(color: Colors.white, width: 2),
+        ),
+        child: Text(
+          'Register now',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _title() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        // text: '$title',
+        style: GoogleFonts.portLligatSans(
+          // textStyle: Theme.of(context).textTheme.headline1,
+          fontSize: 30,
+          fontWeight: FontWeight.w700,
+          color: Colors.white,
+        ),
+        children: [
+          TextSpan(
+            text: 'Food',
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
+          TextSpan(
+            text: 'ga',
+            style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
+          TextSpan(
+            text: 'sm',
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,14 +112,19 @@ class _WelcomePageState extends State<WelcomePage> {
               end: Alignment.bottomCenter,
               colors: [
                 primarycolor,
-                Color(0xFFFFCC80),
+                // secondarycolor,
+                Colors.white,
               ],
             ),
           ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[],
+            children: <Widget>[
+              _title(),
+              _signUpButton(),
+              _login(),
+            ],
           ),
         ),
       ),
