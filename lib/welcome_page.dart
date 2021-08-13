9da18fbf1aa5512b63968f7534efff2c0b5dcf31
Wricheek <wricheekadhikari1@gmail.com>
2021-08-13 22:33:56 +0530

    Welcome page

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -17,23 +17,22 @@ class _WelcomePageState extends State<WelcomePage> {
         //  Navigator.push(
         // context, MaterialPageRoute(builder: (context) => LoginPage()));
       },
-      child: Container(
-        width: getDeviceWidth(context),
-        padding: EdgeInsets.symmetric(vertical: 13),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(5)),
-            boxShadow: <BoxShadow>[
-              BoxShadow(
-                  color: Color(0xffdf8e33).withAlpha(100),
-                  offset: Offset(2, 4),
-                  blurRadius: 8,
-                  spreadRadius: 2)
-            ],
-            color: Colors.white),
-        child: Text(
-          'Login',
-          style: TextStyle(fontSize: 20, color: Color(0xfff7892b)),
+      child: SizedBox(
+        width: 300,
+        child: Container(
+          width: getDeviceWidth(context),
+          padding: EdgeInsets.symmetric(vertical: 13),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+            border: Border.all(color: Colors.white, width: 5),
+          ),
+          child: Text(
+            'Login',
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: primarycolor),
+          ),
         ),
       ),
     );
@@ -45,47 +44,59 @@ class _WelcomePageState extends State<WelcomePage> {
         //  Navigator.push(
         // context, MaterialPageRoute(builder: (context) => SignUpPage()));
       },
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.symmetric(vertical: 13),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(5)),
-          border: Border.all(color: Colors.white, width: 2),
-        ),
-        child: Text(
-          'Register now',
-          style: TextStyle(fontSize: 20, color: Colors.white),
+      child: SizedBox(
+        width: 300,
+        child: Container(
+          width: getDeviceWidth(context),
+          padding: EdgeInsets.symmetric(vertical: 13),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+            border: Border.all(color: Colors.white, width: 5),
+          ),
+          child: Text(
+            'Register now',
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: primarycolor),
+          ),
         ),
       ),
     );
   }
 
   Widget _title() {
-    return RichText(
-      textAlign: TextAlign.center,
-      text: TextSpan(
-        // text: '$title',
-        style: GoogleFonts.portLligatSans(
-          // textStyle: Theme.of(context).textTheme.headline1,
-          fontSize: 30,
-          fontWeight: FontWeight.w700,
-          color: Colors.white,
+    return SizedBox(
+      width: getDeviceWidth(context),
+      child: Container(
+        child: RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            style: GoogleFonts.portLligatSans(
+              fontSize: 200,
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+            ),
+            children: [
+              TextSpan(
+                text: 'Fo',
+                style: TextStyle(color: Colors.black, fontSize: 30),
+              ),
+              TextSpan(
+                text: 'od',
+                style: TextStyle(color: Colors.white, fontSize: 30),
+              ),
+              TextSpan(
+                text: 'ga',
+                style: TextStyle(color: Colors.black, fontSize: 30),
+              ),
+              TextSpan(
+                text: 'sm',
+                style: TextStyle(color: Colors.white, fontSize: 30),
+              ),
+            ],
+          ),
         ),
-        children: [
-          TextSpan(
-            text: 'Food',
-            style: TextStyle(color: Colors.black, fontSize: 30),
-          ),
-          TextSpan(
-            text: 'ga',
-            style: TextStyle(color: Colors.white, fontSize: 30),
-          ),
-          TextSpan(
-            text: 'sm',
-            style: TextStyle(color: Colors.black, fontSize: 30),
-          ),
-        ],
       ),
     );
   }
@@ -95,7 +106,7 @@ class _WelcomePageState extends State<WelcomePage> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.all(getDeviceHeight(context) * 0.8 * 0.5),
+          // padding: EdgeInsets.all(getDeviceHeight(context) * 0.8 * 0.5),
           height: getDeviceHeight(context),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(5)),
@@ -112,8 +123,7 @@ class _WelcomePageState extends State<WelcomePage> {
               end: Alignment.bottomCenter,
               colors: [
                 primarycolor,
-                // secondarycolor,
-                Colors.white,
+                Colors.white70,
               ],
             ),
           ),
@@ -122,8 +132,17 @@ class _WelcomePageState extends State<WelcomePage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               _title(),
-              _signUpButton(),
+              SizedBox(
+                height: 20,
+              ),
               _login(),
+              SizedBox(
+                height: 20,
+              ),
+              _signUpButton(),
+              SizedBox(
+                height: 20,
+              ),
             ],
           ),
         ),
